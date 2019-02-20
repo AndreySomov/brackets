f0dc94136468efccf730184ff9a00441b773400d
AndreySomov <46813938+AndreySomov@users.noreply.github.com>
2019-02-20 10:39:10 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let open = [], close = [];
+
+	bracketsConfig.forEach(item=>{
+		open.push(item[0]);
+		close.push(item[1]);
+	})
+
+	let arr = str.split("");
+	
+	for(let i=1; i<arr.length; i++){
+
+		let cloneI = i-1;
+		if(close.includes(arr[i])){
+
+			while(cloneI>=0){
+				if(arr[cloneI]){
+					if(open.indexOf(arr[cloneI]) == close.indexOf(arr[i])){
+						arr[cloneI] = 0;
+						arr[i] = 0;
+					}
+					break;
+				}
+				cloneI--;
+			}
+		}
+	}
+	
+	return arr.reduce((a, b) => a + b) === 0 ? true : false;
 }
